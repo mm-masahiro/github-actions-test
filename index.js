0805c78931e140dfd53499293f4758fabd52e5dd
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-29 00:19:52 +0900

    fix lint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = function fizzbuzz(value) {
   if (value % 15 === 0) {
-    return 'FizzBuzz';
-  };
+    return "FizzBuzz";
+  }
 
   if (value % 3 === 0) {
-    return 'Fizz';
-  };
+    return "Fizz";
+  }
 
   if (value % 5 === 0) {
-    return 'Buzz';
-  };
+    return "Buzz";
+  }
 
   return String(value);
-}
+};
--- test/index.js
+++ test/index.js
@@ -2,20 +2,20 @@ const assert = require("assert");
 
 const fizzbuzz = require("../index.js");
 
-describe("fizzbuzz", () =>  {
-  it ("returns FizzBuzz when value is devisible by 15", () => {
+describe("fizzbuzz", () => {
+  it("returns FizzBuzz when value is devisible by 15", () => {
     assert(fizzbuzz(30) === "FizzBuzz");
   });
 
-  it ("return Fizz when value is divisible by 3", () => {
+  it("return Fizz when value is divisible by 3", () => {
     assert(fizzbuzz(9) === "Fizz");
   });
 
-  it ("return Buzz when value is divisible by 5", () => {
+  it("return Buzz when value is divisible by 5", () => {
     assert(fizzbuzz(10) === "Buzz");
   });
 
-  it ("return the value when value is not divisible by 3 or 5", () => {
+  it("return the value when value is not divisible by 3 or 5", () => {
     assert(fizzbuzz(7) === "7");
   });
-})
+});
